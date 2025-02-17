b9386294f3bcc4d149efe79bc8e695e5f7000499
Alejandro CR <contacto@alejandrocr.co>
2025-02-17 04:58:07 -0500

    Configure MikroORM in AppModule with SQLite database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SharedModule } from './shared/shared.module';
 import { DatabaseModule } from './database/database.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { SqliteDriver } from '@mikro-orm/sqlite';
+
 
 @Module({
-  imports: [UsersModule, SharedModule],
+  imports: [
+    UsersModule,
+    SharedModule,
+    DatabaseModule,
+    MikroOrmModule.forRoot({
+      entities: ['./dist/users/'],
+      entitiesTs: ['./src/users/'],
+      dbName: 'db.sqlite3',
+      driver: SqliteDriver,
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
