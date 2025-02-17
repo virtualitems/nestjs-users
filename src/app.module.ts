86a1d6db18550ea2831cf0f1975a2a3cdee3d286
Alejandro CR <contacto@alejandrocr.co>
2025-02-17 04:46:33 -0500

    Add DatabaseModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SharedModule } from './shared/shared.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [UsersModule, SharedModule],
