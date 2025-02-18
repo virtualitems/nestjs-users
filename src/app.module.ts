0aa0a1c282a69643a249e8daef96ae9e987fe90d
Alejandro CR <contacto@alejandrocr.co>
2025-02-17 21:10:19 -0500

    Remove unused DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     AuthModule,
-    DatabaseModule,
     MikroOrmModule.forRoot(),
   ],
   controllers: [AppController],
