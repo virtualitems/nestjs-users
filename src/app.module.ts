5629840b3b5e49c6011f7c43410e8d298b3780a3
Alejandro CR <contacto@alejandrocr.co>
2025-02-18 21:12:43 -0500

    Add @nestjs/config package and configure environment variables in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import config from './mikro-orm.config';
+import { routes } from './routes';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [routes],
+    }),
     MikroOrmModule.forRoot(config),
     AuthModule,
   ],
