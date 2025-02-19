6944c6b0a0123c96ee1d6fef9e9c964b5ba93fdb
Alejandro CR <contacto@alejandrocr.co>
2025-02-19 03:26:30 -0500

    Update JWT strategy to use environment variable for secret key

--- src/jwt/jwt.strategy.ts
+++ src/jwt/jwt.strategy.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from '../auth/auth.service';
-
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret',
+      secretOrKey: 'JWT_SECRET',
     });
   }
 
