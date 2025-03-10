e7b1d2b4b7f9e35eb6068d1b189a2f2f5fef259e
Alejandro CR <contacto@alejandrocr.co>
2025-03-10 03:41:42 -0500

    add JwtPayload interface for JWT token structure

---
+++ src/auth/jwt/jwt.interface.ts
@@ -0,0 +1,11 @@
+export interface JwtPayload {
+  iss?: string; // issuer of the jwt
+  sub?: number; // subject of the jwt (the user)
+  aud?: string; // recipient for which the jwt is intended
+  exp?: number; // time after which the jwt expires
+  nbf?: number; // time before which the jwt must not be accepted for processing
+  iat?: number; // time at which the jwt was issued; can be used to determine age of the jwt
+  jti?: number; // unique identifier; can be used to prevent the jwt from being replayed (allows a token to be used only once)
+  ugs?: string; // user groups
+  pms?: string; // permissions
+}
