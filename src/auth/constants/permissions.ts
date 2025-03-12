97cf8c939e1b0c8a381339ab4b408435aa3b693e
Alejandro CR <contacto@alejandrocr.co>
2025-03-12 02:34:54 -0500

    feat: add permissions enum for user and group management

---
+++ src/auth/constants/permissions.ts
@@ -0,0 +1,16 @@
+export enum permissions {
+  // users
+  USERS_LIST = 'users.list',
+  USERS_SHOW = 'users.show',
+  USERS_CREATE = 'users.create',
+  USERS_UPDATE = 'users.update',
+  USERS_DELETE = 'users.delete',
+  USERS_RESTORE = 'users.restore',
+  // groups
+  GROUPS_LIST = 'groups.list',
+  GROUPS_SHOW = 'groups.show',
+  GROUPS_CREATE = 'groups.create',
+  GROUPS_UPDATE = 'groups.update',
+  GROUPS_DELETE = 'groups.delete',
+  GROUPS_RESTORE = 'groups.restore',
+}
