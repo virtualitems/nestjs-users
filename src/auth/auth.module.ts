0116c3a0865fbaccfed2f2fa207422d5f73202bd
Alejandro CR <contacto@alejandrocr.co>
2025-03-22 00:02:55 -0500

    refactor: remove unused jwt references from auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 
 import { RolesController } from './controllers/roles.controller';
 import { UsersController } from './controllers/users.controller';
@@ -14,11 +14,9 @@ import { UserPermission } from './entities/user-permission.entity';
 import { User } from './entities/user.entity';
 
 import { RolesService } from './providers/roles.service';
-import { JwtStrategy } from '../shared/providers/jwt.strategy';
 import { HashingService } from './providers/hashing.service';
 import { UsersService } from './providers/users.service';
 import { PermissionsService } from './providers/permissions.service';
-import { PermissionsGuard } from '../shared/providers/jwt.guard';
 import { PermissionsController } from './controllers/permissions.controller';
 
 @Module({
@@ -42,15 +40,7 @@ import { PermissionsController } from './controllers/permissions.controller';
     }),
   ],
   controllers: [UsersController, RolesController, PermissionsController],
-  providers: [
-    HashingService,
-    JwtService,
-    JwtStrategy,
-    PermissionsGuard,
-    PermissionsService,
-    RolesService,
-    UsersService,
-  ],
+  providers: [HashingService, PermissionsService, RolesService, UsersService],
   exports: [UsersService, RolesService, PermissionsService],
 })
 export class AuthModule {}
