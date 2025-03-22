990703bc931a21a7968b6371619709d47b197878
Alejandro CR <contacto@alejandrocr.co>
2025-03-22 03:57:13 -0500

    export multer config as factory function

--- src/multer.config.ts
+++ src/multer.config.ts
@@ -1,25 +1,27 @@
-import { join } from 'node:path';
 import { Request } from 'express';
 import { diskStorage } from 'multer';
 
-export const uploadPath = join(__dirname, '..', 'media');
+function filename(
+  req: Request,
+  file: Express.Multer.File,
+  callback: (error: Error | null, filename: string) => void,
+) {
+  const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+  const filename = uniqueSuffix + '-' + file.originalname;
+  callback(null, filename);
+}
 
-export const diskStorageConfiguration = {
-  destination: uploadPath,
-  filename: (
-    req: Request,
-    file: Express.Multer.File,
-    callback: (error: Error | null, filename: string) => void,
-  ) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const filename = uniqueSuffix + '-' + file.originalname;
-    callback(null, filename);
-  },
-};
-
-export const multerConfiguration = {
-  storage: diskStorage(diskStorageConfiguration),
-  limits: {
-    fileSize: 1024 * 1024 * 5, // 5MB
-  },
-};
+export function multerDiskStorage(
+  diskStorageDestination: string,
+  maxFileSize?: number,
+) {
+  return {
+    storage: diskStorage({
+      destination: diskStorageDestination,
+      filename: filename,
+    }),
+    limits: {
+      fileSize: maxFileSize ?? 1024 * 1024 * 5, // 5MB
+    },
+  };
+}
