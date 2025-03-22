19a661cbaecbf4e04aae4a6d54b189bdb58e58a0
Alejandro CR <contacto@alejandrocr.co>
2025-03-21 23:29:27 -0500

    refactor: simplify ConfigModule initialization in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,22 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { AuthPersonsModule } from './auth-persons/auth-persons.module';
 import mikroOrmConfig from './mikro-orm.config';
 
+const configModuleOptions = {
+  isGlobal: true,
+  envFilePath: '.env',
+};
+
 @Module({
   imports: [
     AuthModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
+    ConfigModule.forRoot(configModuleOptions),
     MikroOrmModule.forRoot(mikroOrmConfig),
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.getOrThrow('JWT_SECRET'),
-        signOptions: { expiresIn: configService.getOrThrow('JWT_EXPIRES_IN') },
-      }),
-    }),
     AuthPersonsModule,
   ],
   controllers: [AppController],
