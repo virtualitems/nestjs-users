3f4d3532039f41aec61f998d073800022f3e61fb
Alejandro CR <contacto@alejandrocr.co>
2025-03-22 22:05:16 -0500

    refactor: replace ConfigService with dotenv for JWT configuration

--- src/auth-persons/auth-persons.module.ts
+++ src/auth-persons/auth-persons.module.ts
@@ -1,6 +1,5 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 
 import { AuthModule } from '../auth/auth.module';
@@ -9,6 +8,7 @@ import { UsersService } from '../auth/providers/users.service';
 import { Person } from '../persons/entities/person.entity';
 import { PersonsModule } from '../persons/persons.module';
 import { PersonsService } from '../persons/providers/persons.service';
+import { env } from '../shared/dotenv';
 import { HashingService } from '../shared/providers/hashing.service';
 import { AuthPersonsController } from './controllers/auth-persons.controller';
 import { PersonUser } from './entities/persons-users.entity';
@@ -16,12 +16,9 @@ import { AuthPersonsService } from './providers/auth-persons.service';
 
 @Module({
   imports: [
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.getOrThrow('JWT_SECRET'),
-        signOptions: { expiresIn: configService.getOrThrow('JWT_EXPIRES_IN') },
-      }),
+    JwtModule.register({
+      secret: env.JWT_SECRET,
+      signOptions: { expiresIn: env.JWT_EXPIRES_IN },
     }),
     AuthModule,
     PersonsModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { JwtStrategy } from 'src/shared/providers/jwt.strategy';
 
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 
+import { env } from '../shared/dotenv';
+import { HashingService } from '../shared/providers/hashing.service';
 import { PermissionsController } from './controllers/permissions.controller';
 import { RolesController } from './controllers/roles.controller';
 import { UsersController } from './controllers/users.controller';
@@ -14,19 +15,15 @@ import { Role } from './entities/role.entity';
 import { UserPermission } from './entities/user-permission.entity';
 import { UserRole } from './entities/user-role.entity';
 import { User } from './entities/user.entity';
-import { HashingService } from '../shared/providers/hashing.service';
 import { PermissionsService } from './providers/permissions.service';
 import { RolesService } from './providers/roles.service';
 import { UsersService } from './providers/users.service';
 
 @Module({
   imports: [
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.getOrThrow('JWT_SECRET'),
-        signOptions: { expiresIn: configService.getOrThrow('JWT_EXPIRES_IN') },
-      }),
+    JwtModule.register({
+      secret: env.JWT_SECRET,
+      signOptions: { expiresIn: env.JWT_EXPIRES_IN },
     }),
     MikroOrmModule.forFeature({
       entities: [
