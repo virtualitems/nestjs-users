0c5b83bee22fd873f6135b3e0af2cff19a94c2da
Alejandro CR <contacto@alejandrocr.co>
2025-04-14 16:18:51 -0500

    feat: add CSRF hash generation method to HashingService

--- src/shared/providers/hashing.service.ts
+++ src/shared/providers/hashing.service.ts
@@ -1,5 +1,7 @@
 import * as bcryptjs from 'bcryptjs';
 
+import * as crypto from 'node:crypto';
+
 import { Injectable } from '@nestjs/common';
 
 import { env } from '../env';
@@ -12,6 +14,12 @@ export class HashingService {
     const rounds = Number(env.PASSWORD_HASH_SALT_ROUNDS);
     const salt = bcryptjs.genSaltSync(rounds);
     return bcryptjs.hashSync(text, salt);
+  }
+
+  public csrf(...components: string[]): string {
+    const hash = crypto.createHash('sha256');
+    hash.update(components.join(';'));
+    return hash.digest('hex');
   }
 
   public compare(text: string, hash: string): boolean {
